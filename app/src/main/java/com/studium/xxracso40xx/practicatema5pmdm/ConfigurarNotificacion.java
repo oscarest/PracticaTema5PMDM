a5762dfbfceb72f7120ea7943fea5bd442a8e33a
Xxracso40xX <Xxracso40xX@gmail.com>
2019-01-03 16:00:55 +0100

    Realizado botón 2 excepto que la notificación se abre en el momento que se pulsa el botón.

--- app/src/main/java/com/studium/xxracso40xx/practicatema5pmdm/ConfigurarNotificacion.java
+++ app/src/main/java/com/studium/xxracso40xx/practicatema5pmdm/ConfigurarNotificacion.java
@@ -1,13 +1,52 @@
 package com.studium.xxracso40xx.practicatema5pmdm;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ConfigurarNotificacion extends AppCompatActivity {
+    AppCompatActivity estaActividad;
+    Button boton;
+    EditText edit1;
+    EditText edit2;
+    EditText edit3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_notificacion);
+        estaActividad = this;
+        boton = findViewById(R.id.button3);
+        edit1 = findViewById(R.id.editText4);
+        edit2 = findViewById(R.id.editText5);
+        edit3 = findViewById(R.id.editText6);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PendingIntent i = PendingIntent.getActivity(estaActividad, 0, getIntent(), 0);
+
+                NotificationCompat.Builder mensaje = new NotificationCompat.Builder(estaActividad, "CH_ID");
+                mensaje.setAutoCancel(true)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setWhen(System.currentTimeMillis() + (Integer.parseInt(edit3.getText().toString()) * 1000))
+                        //.setWhen(System.currentTimeMillis() + AlarmManager.ELAPSED_REALTIME * Integer.parseInt(edit3.getText().toString()))
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setPriority(Notification.PRIORITY_MAX) // No válido en API 26
+                        .setContentTitle(edit1.getText())
+                        .setContentText(edit2.getText())
+                        .setContentIntent(i);
+                NotificationManager nM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                nM.notify(1, mensaje.build());
+            }
+        });
     }
 }
