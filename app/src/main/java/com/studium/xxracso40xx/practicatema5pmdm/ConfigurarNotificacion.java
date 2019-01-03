04a59f0fba0c5ebdd51c6c0174faefd967ecab29
Xxracso40xX <Xxracso40xX@gmail.com>
2019-01-03 02:52:59 +0100

    Creación de las diferentes activities y de los botones en la activity incicial. Realizando la funcionalidad para que los botones abran las distintas activity.

---
+++ app/src/main/java/com/studium/xxracso40xx/practicatema5pmdm/ConfigurarNotificacion.java
@@ -0,0 +1,13 @@
+package com.studium.xxracso40xx.practicatema5pmdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigurarNotificacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurar_notificacion);
+    }
+}
--- app/src/main/java/com/studium/xxracso40xx/practicatema5pmdm/Inicio.java
+++ app/src/main/java/com/studium/xxracso40xx/practicatema5pmdm/Inicio.java
@@ -1,13 +1,37 @@
 package com.studium.xxracso40xx.practicatema5pmdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
-
+Button boton1;
+Button boton2;
+Intent intent1;
+Intent intent2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inicio);
+        boton1 = findViewById(R.id.button);
+        boton2 = findViewById(R.id.button1);
+        intent1 = new Intent(this, ConfigurarToast.class);
+        intent2 = new Intent(this, ConfigurarNotificacion.class);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(intent1);
+            }
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(intent2);
+            }
+        });
     }
 }
